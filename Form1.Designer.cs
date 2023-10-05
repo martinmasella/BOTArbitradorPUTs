acc832e37d889dc407408d3dbdd913c38794b5b2
Martin Masella <pccito@yahoo.com>
2023-10-05 17:39:40 -0300

    Code refactor

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -246,7 +246,6 @@
             this.Controls.Add(this.grbLogin);
             this.Name = "frmMain";
             this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
             this.Load += new System.EventHandler(this.frmMain_Load);
             this.grbLogin.ResumeLayout(false);
             this.grbLogin.PerformLayout();
